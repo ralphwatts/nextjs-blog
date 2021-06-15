53adafd3499cf60b1533e7e706893d2de2a34810
DI <DI@DOCTORSINTERNET.local>
2021-06-15 12:29:00 -0400

    deployment

--- backup-6-14/pages/index.js
+++ backup-6-14/pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -26,14 +24,12 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>          
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
           ))}
         </ul>
       </section>
